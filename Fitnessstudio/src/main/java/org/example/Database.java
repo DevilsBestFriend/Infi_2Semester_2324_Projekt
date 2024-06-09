8e68243b86955b4a5c216b1ed94954b085bb5e00
Peter Quickner <peteryt690@gmail.com>
2024-06-09 23:18:06 +0200

    format rs

--- Fitnessstudio/src/main/java/org/example/Database.java
+++ Fitnessstudio/src/main/java/org/example/Database.java
@@ -26,13 +26,59 @@ public class Database {
     }
 
     public void execStmt(String stmt) throws SQLException {
-            Statement st = con.createStatement();
-            st.execute(stmt);
+        Statement st = con.createStatement();
+        st.execute(stmt);
     }
 
-    public ResultSet execQuery(String query) throws SQLException {
-        Statement st = con.createStatement();
-        return st.executeQuery(query);
+    public ResultSet execQuery(String query, boolean prepared) throws SQLException {
+        if (!prepared) {
+            Statement st = con.createStatement();
+            return st.executeQuery(query);
+        } else {
+            PreparedStatement pst = con.prepareStatement(query);
+            return pst.executeQuery();
+        }
+    }
+
+    public static void formatResult(ResultSet rs) throws SQLException {
+        ResultSetMetaData rsmd = rs.getMetaData();
+        String[] columnNames = new String[((ResultSetMetaData) rsmd).getColumnCount()];
+        for (int i = 1; i <= columnNames.length; i++) {
+            columnNames[i] = rsmd.getColumnName(i);
+        }
+
+        try {
+            while (rs.next()) {
+                int i = 1;
+                for (String columnName : columnNames) {
+                    int type = rsmd.getColumnType(i);
+                    switch (type) {
+                        case java.sql.Types.INTEGER:
+                            System.out.println(columnName + ": " + rs.getInt(columnName));
+                            break;
+                        case java.sql.Types.DOUBLE:
+                            System.out.println(columnName + ": " + rs.getDouble(columnName));
+                            break;
+                        case java.sql.Types.DATE:
+                            System.out.println(columnName + ": " + rs.getDate(columnName));
+                            break;
+                        case java.sql.Types.TIME:
+                            System.out.println(columnName + ": " + rs.getTime(columnName));
+                            break;
+                        case java.sql.Types.BOOLEAN:
+                            System.out.println(columnName + ": " + rs.getBoolean(columnName));
+                            break;
+                        default:
+                            System.out.println(columnName + ": " + rs.getString(columnName));
+                            break;
+                    }
+                    i++;
+                }
+            }
+        } catch (
+                SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void close() {
