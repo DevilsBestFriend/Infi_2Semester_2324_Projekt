525a932691e4c299a19a93e6aec4ec21ad756121
Peter Quickner <peteryt690@gmail.com>
2024-06-09 17:17:08 +0200

    tabellen

--- Fitnessstudio/src/main/java/org/example/Main.java
+++ Fitnessstudio/src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 //
-// TODO DB STARTEN NICHT VERGESSEN
+// DB STARTEN NICHT VERGESSEN
 //
 public class Main {
     public static void main(String[] args) {
@@ -28,11 +28,13 @@ public class Main {
 
 
     public static void dbinit(Database db) {
+        //CREATE TYPE wochentag AS ENUM ('Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag');
         //CREATE TYPE mitgliedschaft AS ENUM ('Schüler', 'Standard', 'Premium', 'VIP', 'Gast', 'Abgemeldet');
         //in psql commandline einfügen
+
+        //erstellung der Tabellen
         db.execStmt("CREATE TABLE IF NOT EXISTS mitglieder (" +
                 "id UUID PRIMARY KEY NOT NULL, " +
-                "hid INT" +
                 "vname VARCHAR(255) , " +
                 "nname VARCHAR(255) , " +
                 "gbd DATE, " +
@@ -40,7 +42,6 @@ public class Main {
                 "vertragsstart DATE NOT NULL DEFAULT CURRENT_DATE)");
         db.execStmt("CREATE TABLE IF NOT EXISTS trainer (" +
                 "id UUID PRIMARY KEY NOT NULL, " +
-                "hid INT" +
                 "vname VARCHAR(255) , " +
                 "nname VARCHAR(255) , " +
                 "gbd DATE, " +
@@ -49,10 +50,16 @@ public class Main {
         db.execStmt("CREATE TABLE IF NOT EXISTS kurs (" +
                 "id UUID PRIMARY KEY NOT NULL, " +
                 "name VARCHAR(255) , " +
-                "trainer INT, " +
-                "startzeit TIME NOT NULL, " +
-                "endzeit TIME NOT NULL, " +
-                "tag INT NOT NULL,)");
-        // TODO FK trainer  und eigene Tabelle für teilnehmer, Start und endzeit, tag soll in start und endziet eingebunden sein
+                "trainer UUID, " +
+                "startzeit time, " +
+                "endzeit time, " +
+                "tag WOCHENTAG NOT NULL," +
+                "FOREIGN KEY (trainer) REFERENCES trainer(id))");
+        db.execStmt("CREATE TABLE IF NOT EXISTS teilnehmer (" +
+                "id UUID PRIMARY KEY NOT NULL, " +
+                "mitglied UUID, " +
+                "kurs UUID, " +
+                "FOREIGN KEY (mitglied) REFERENCES mitglieder(id), " +
+                "FOREIGN KEY (kurs) REFERENCES kurs(id))");
     }
 }
