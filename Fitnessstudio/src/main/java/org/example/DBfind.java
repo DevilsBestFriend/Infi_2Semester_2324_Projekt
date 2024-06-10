09c61487fe2dfa92bd3e56885eb998d4b16e316c
Peter Quickner <peteryt690@gmail.com>
2024-06-10 20:07:30 +0200

    find kurs

--- Fitnessstudio/src/main/java/org/example/DBfind.java
+++ Fitnessstudio/src/main/java/org/example/DBfind.java
@@ -1,34 +1,49 @@
 package org.example;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class DBfind {
+    private static Scanner scn = new Scanner(System.in);
+
     public static void findmitglied(Database db) throws SQLException {
-        ResultSet rs = db.execQuery("SELECT vname Vorname, nname Nachname, gbd Geburtsdatum, " +
-                "mitgliedschft Mitgliedschaft, checkin Checkin, vertragsstart 'Mitglied seit'" +
-                "FROM mitglieder ORDER BY nname ASC", true);
+        ResultSet rs = db.execQueryno("SELECT vname Vorname, nname Nachname, gbd Geburtsdatum, " +
+                "mitgliedschaft Mitgliedschaft, checkin Checkin, vertragsstart \"Mitglied seit\" " +
+                "FROM mitglieder ORDER BY nname ASC");
         Database.formatResult(rs);
     }
 
     public static void findtrainer(Database db) throws SQLException {
-        ResultSet rs = db.execQuery("SELECT vname Vorname, nname Nachname, gbd Geburtsdatum, gehalt Gehalt, einstelldatum Einstelldatum " +
-                "FROM trainer ORDER BY nname ASC", true);
+        ResultSet rs = db.execQueryno("SELECT vname Vorname, nname Nachname, gbd Geburtsdatum, gehalt Gehalt, einstelldatum Einstelldatum " +
+                "FROM trainer ORDER BY nname ASC");
         Database.formatResult(rs);
     }
 
     public static void findkurs(Database db) throws SQLException {
-        ResultSet rs = db.execQuery("SELECT kurs.name Kursname, trainer.nname Trainer, startzeit Startzeit, endzeit Endzeit, tag Tag" +
-                "FROM kurs INNER JOIN trainer ON kurs.trainer = trainer.id" +
-                "GROUP BY kurs.tag order by kurs.tag ASC", true);
-        Database.formatResult(rs);
+        System.out.println("Enter course name");
+        String name = scn.nextLine();
+
+        PreparedStatement preparedStatement = db.getCon().prepareStatement("SELECT k.tag Tag, k.name Kursname, t.nname Trainer, k.startzeit Startzeit, k.endzeit Endzeit " +
+                "FROM kurs k INNER JOIN trainer t ON k.trainer = t.id " +
+                "WHERE k.name LIKE ? " +
+                "ORDER BY k.tag ASC");
+        preparedStatement.setString(1, "%"+name+"%");
+        try {
+            ResultSet rs = db.execQueryye(preparedStatement);
+            Database.formatResult(rs);
+        } catch (SQLException e) {
+            System.out.println("Fehler beim Suchen des Kurses finde ich");
+            e.printStackTrace();
+        }
     }
 
     public static void findteilnehmer(Database db) throws SQLException {
-        ResultSet rs = db.execQuery("SELECT kurs.name Kurs, mitglieder.nname Nachname, mitglieder.vname Vorname, mitligeder.checkin Checkin" +
-                "FROM teilnehmer INNER JOIN mitglieder ON teilnehmer.mitglied = mitglieder.id " +
-                "INNER JOIN kurs ON teilnehmer.kurs = kurs.id" +
-                "GROUP BY kurs.name ORDER BY kurs.name ASC", true);
+        ResultSet rs = db.execQueryno("SELECT k.name Kurs, m.nname \"Teilnehmer n\", m.vname \"Teilnehmer v\", m.checkin Checkin " +
+                "FROM teilnehmer t INNER JOIN mitglieder m ON t.mitglied = m.id " +
+                "INNER JOIN kurs k ON t.kurs = k.id " +
+                "ORDER BY k.name ASC");
         Database.formatResult(rs);
     }
 }
