bcdc236db9a379be767ced7f082aaab9d2194295
Peter Quickner <peteryt690@gmail.com>
2024-06-11 22:25:01 +0200

    backup :(

--- Fitnessstudio/src/main/java/org/example/Database.java
+++ Fitnessstudio/src/main/java/org/example/Database.java
@@ -1,6 +1,11 @@
 package org.example;
 
+import java.io.StringWriter;
 import java.sql.*;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
 
 public class Database {
     Connection con;
@@ -17,14 +22,73 @@ public class Database {
         }
     }
 
-    public void restore() throws SQLException {
+    public void restore() {
         // TODO
         // Store file in src/main/resources
     }
 
-    public void backup() throws SQLException {
+    public void backup() {
         // TODO
         // Store file in src/main/resources
+        FileWriter file = null;
+        try {
+            // initialize File writer to write the backup to a file and remove previous backup
+            file = new FileWriter("src/main/resources/backup.json");
+            file.flush();
+
+            JSONObject jsonObject = null;
+
+            // sql for tables
+            String[] tablessql = {"SELECT * FROM mitglieder", "SELECT * FROM trainer", "SELECT * FROM kurs", "SELECT * FROM teilnehmer"};
+
+            for (int i = 0; i < tablessql.length; i++) {
+                jsonObject = new JSONObject();
+                ResultSet rs = execQueryno(tablessql[i]);
+                ResultSetMetaData rsmd = rs.getMetaData();
+                String[] columnNames = new String[rsmd.getColumnCount()];
+                for (int j = 0; j < columnNames.length; j++) {
+                    columnNames[j] = rsmd.getColumnName(j + 1);
+                }
+                while (rs.next()) {
+                    int j = 1;
+                    for (String columnName : columnNames) {
+                        int type = rsmd.getColumnType(j);
+                        switch (type) {
+                            case java.sql.Types.INTEGER:
+                                jsonObject.put(columnName, rs.getInt(columnName));
+                                break;
+                            case java.sql.Types.DOUBLE:
+                                jsonObject.put(columnName, rs.getDouble(columnName));
+                                break;
+                            /*case java.sql.Types.BOOLEAN:
+                                jsonObject.put(columnName, rs.getBoolean(columnName));
+                                break;*/
+                            default:
+                                jsonObject.put(columnName, rs.getString(columnName));
+                                break;
+                        }
+                        j++;
+                    }
+                    jsonObject.writeJSONString(file);
+                }
+
+                String jsonText = file.toString();
+                System.out.print(jsonText);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("File not found");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("SQL error");
+        } finally {
+            try {
+                file.close();
+            } catch (IOException ignored) {
+            }
+        }
+
     }
 
     public void execStmt(String stmt) throws SQLException {
