886992bbd34a1c6594db887492c1825a738ccaf5
Peter Quickner <peteryt690@gmail.com>
2024-06-11 22:45:21 +0200

    functions

--- Fitnessstudio/src/main/java/org/example/DBdelete.java
+++ Fitnessstudio/src/main/java/org/example/DBdelete.java
@@ -2,4 +2,15 @@ package org.example;
 
 public class DBdelete
 {
+    public static void delmitglied(Database db) {
+    }
+
+    public static void deltrainer(Database db) {
+    }
+
+    public static void delkurs(Database db) {
+    }
+
+    public static void delteilnehmer(Database db) {
+    }
 }
--- Fitnessstudio/src/main/java/org/example/DBupdate.java
+++ Fitnessstudio/src/main/java/org/example/DBupdate.java
@@ -2,4 +2,15 @@ package org.example;
 
 public class DBupdate {
 
+    public static void updteilnehmer(Database db) {
+    }
+
+    public static void updkurs(Database db) {
+    }
+
+    public static void updmitglied(Database db) {
+    }
+
+    public static void updtrainer(Database db) {
+    }
 }
